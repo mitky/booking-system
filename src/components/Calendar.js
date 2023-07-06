7fb8da5af5456168db8a1749395bf85a6a9d3a15
mitky <dimitar.shikrenov@gmail.com>
2023-07-06 11:08:11 +0100

    get user info for username

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,14 +2,39 @@ import React from "react";
 import '../App.css';
 import client from './Client';
 import { usernameinfo} from '../App';
+import {  gql } from '@apollo/client';
 
 
 export default function Clanedar(){
+
+    const GET_User_Info = gql`
+    query GetPassword($username: String!) {
+        appointment_project(where: {username: {_eq: $username}}) {
+        password
+      }
+    }
+  `;
+
+
     
     function getInfo() {
-        alert(usernameinfo);
-       
-      }
+
+        const username = usernameinfo;
+        client.query({
+            query: GET_User_Info,
+            variables: { username },
+        })
+            .then(response => {
+             const password = response.data.appointment_project[0].password;
+             alert(password);
+             console.log('Password:', password);
+  })
+            .catch(error => {
+            alert("Error");
+            console.error('Error:', error);
+  });
+
+    }
 
     return(
         <div>
